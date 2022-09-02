3670762a2c83389fc2e949f95f1c261f309d54fc
gunny6026 <gunny6026@naver.com>
2022-09-02 15:59:31 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {Routes , Route} from 'react-router-dom'
+import {Container} from 'react-bootstrap'
+ 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Container>Hi</Container>
+  )
+}
+
+export default App
